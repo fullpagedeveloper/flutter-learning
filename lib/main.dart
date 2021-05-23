8e5a54a361fc476f2e6587caf672d814c65ffbb0
fullpagedeveloper <fullpagedeveloper@gmail.com>
2021-05-24 00:52:16 +0700

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//margin: EdgeInsets.fromLTRB(10, 20, 30, 40)
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // column
-          crossAxisAlignment: CrossAxisAlignment.start, //row
-          children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: <Widget>[Text('data'), Text('data 2')],
-            )
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Latihan Row dan Column"),
+          ),
+          body: Container(
+            color: Colors.red,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Container(
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[Colors.amber, Colors.blue],
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
