cfe078982b2aa94fc61612e3351f663e64d5f8bb
fullpagedeveloper <fullpagedeveloper@gmail.com>
2021-06-01 01:55:20 +0700

    learning stack and align

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,99 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
+          title: Text("Stact and Align"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // column
-          crossAxisAlignment: CrossAxisAlignment.start, //row
+        body: Stack(
           children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: <Widget>[Text('data'), Text('data 2')],
-            )
+            //background
+            Column(
+              children: <Widget>[
+                //satu banding satu menggunakan Flexible()
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.white),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.black12),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.black12),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            //listview
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "Ini text yang ada di  lapis kedua di  stack",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            //button , Aligm untuk memindahkan button kemanapun diletakan
+            Align(
+              alignment: Alignment(0, 0.80), //ini custom pisiisi
+              child: RaisedButton(
+                  child: Text("My button"),
+                  color: Colors.amber,
+                  onPressed: () {}),
+            ),
           ],
         ),
       ),
