26d87adb090f9ce455dccd88512543fb3dda6af7
fullpagedeveloper <fullpagedeveloper@gmail.com>
2021-06-04 05:21:01 +0700

    learing mediaquery

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_row_column/main_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Row dan Column"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // column
-          crossAxisAlignment: CrossAxisAlignment.start, //row
-          children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: <Widget>[Text('data'), Text('data 2')],
-            )
-          ],
-        ),
-      ),
+      home: MainPage(),
     );
   }
 }
